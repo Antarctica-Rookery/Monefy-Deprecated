99e2da2367cbf0fa256405781206918360ff13e2
ando-hunterz <ando.hunterz@gmail.com>
2021-04-27 19:18:10 +0700

    Firebase Init

--- app/src/main/java/com/malisius/monefy/HomeActivity.java
+++ app/src/main/java/com/malisius/monefy/HomeActivity.java
@@ -3,19 +3,29 @@ package com.malisius.monefy;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class HomeActivity extends AppCompatActivity {
     BottomNavigationView bottom_nav;
+    TextView alertTv;
+    FirebaseAuth mAuth;
+    FirebaseDatabase mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance();
 
         bottom_nav = findViewById(R.id.bottom_nav);
         bottom_nav.setSelectedItemId(R.id.home);
@@ -30,5 +40,8 @@ public class HomeActivity extends AppCompatActivity {
                 return false;
             }
         });
+        DatabaseReference userDataRef = mDatabase.getReference().child(mAuth.getCurrentUser().getUid());
+
+        alertTv.setText("There's No Alert");
     }
 }
